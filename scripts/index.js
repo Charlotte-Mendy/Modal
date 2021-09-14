b9537d198c959d3bb58262ab5e0411766465538e
Charlotte Mendy <charlotte.mendy.dev@gmail.com>
2021-09-14 11:48:58 +0200

    Get elements

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+'use-strict';
+
+//& Get elements
+//& -----------------------------------
+
+const btnsOpenModal = document.querySelectorAll('open-modal');
+const modal = document.querySelector('modal');
+const btnCloseModal = document.querySelector('close-modal');
+const overlay = document.querySelector('overlay');
