a1243d93841d01b555710e1f2b8c34ac303d1a80
Charlotte Mendy <charlotte.mendy.dev@gmail.com>
2021-09-14 12:13:09 +0200

    Set functions to open & close the modal

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,34 @@
 //& Get elements
 //& -----------------------------------
 
-const btnsOpenModal = document.querySelectorAll('open-modal');
-const modal = document.querySelector('modal');
-const btnCloseModal = document.querySelector('close-modal');
-const overlay = document.querySelector('overlay');
+const btnsOpenModal = document.querySelectorAll('.open-modal');
+// console.log(btnsOpenModal);
+const modal = document.querySelector('.modal');
+// console.log(modal);
+const btnCloseModal = document.querySelector('.close-modal');
+// console.log(btnCloseModal);
+const overlay = document.querySelector('.overlay');
+// console.log(overlay);
+
+//& Open the modal
+//& -----------------------------------
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+};
+
+// Loop through all open buttons, listen to click event
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  // console.log(btnsOpenModal[i]);
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+//& Close the modal
+//& -----------------------------------
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+};
+
+// Listen to click event
+btnCloseModal.addEventListener('click', closeModal);
